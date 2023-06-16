fb2346e7ecca4e3abbddfb7f2b1fffa79a6338d5
Zeke <soto.zeke5@gmail.com>
2023-06-16 15:12:43 -0500

    day 1 challenge 2 update

--- day1/elf-calories.js
+++ day1/elf-calories.js
@@ -2,13 +2,32 @@ function isPositiveInt (value) {
   return /^\d+$/.test(value)
 }
 
-async function largestElfCalories (file = '') {
+function sortElves (topElves) {
+  if (topElves.sort) {
+    return
+  }
+  for (let i = 0; i < topElves.elves.length; i++) {
+    if (topElves.elves[i] < topElves.smallestMax) {
+      topElves.smallestMax = topElves.elves[i]
+      topElves.smallestMaxIndex = i
+    }
+  }
+  // console.log('Smallest Max Function: ' + topElves.elves[0])
+  topElves.sort = true
+}
+
+async function largestElfCalories (file = '', ofTopNElves = 1) {
   if (!file) {
     return
   }
   const separator = ''
+  const topElves = {
+    smallestMax: 0,
+    smallestMaxIndex: 0,
+    sort: true,
+    elves: new Array(ofTopNElves).fill(0)
+  }
   // let elfNum = 1
-  let maxCalories = 0
   let currentElfCalorieTotal = 0
   const fs = require('node:fs')
   const readline = require('node:readline')
@@ -17,13 +36,17 @@ async function largestElfCalories (file = '') {
     input: fs.createReadStream(file)
   })
   for await (const line of fileInterface) {
-    console.log('Current Line: ' + line)
-    console.log('Max Calorie Count: ' + maxCalories)
+    // console.log('Current Line: ' + line)
     if (line === separator) {
       // elfNum += 1
-      if (currentElfCalorieTotal > maxCalories) {
-        maxCalories = currentElfCalorieTotal
+      await sortElves(topElves)
+      if (currentElfCalorieTotal > topElves.smallestMax) {
+        topElves.elves[topElves.smallestMaxIndex] = currentElfCalorieTotal
+        topElves.smallestMax = currentElfCalorieTotal
+        topElves.sort = false
       }
+      console.log('Elf Calories Count: ' + currentElfCalorieTotal)
+      console.log('Smallest Max:' + topElves.smallestMax)
       currentElfCalorieTotal = 0
     } else {
       if (!isPositiveInt(line)) {
@@ -31,14 +54,28 @@ async function largestElfCalories (file = '') {
         return
       }
       currentElfCalorieTotal += Number(line)
-      console.log('Current Elf Calories Count: ' + currentElfCalorieTotal)
+      // console.log('Current Elf Calories Count: ' + currentElfCalorieTotal)
     }
   }
-  return currentElfCalorieTotal > maxCalories ? currentElfCalorieTotal : maxCalories
+  await sortElves(topElves)
+  if (currentElfCalorieTotal > topElves.smallestMax) {
+    topElves.elves[topElves.smallestMaxIndex] = currentElfCalorieTotal
+  }
+  return topElves.elves
+}
+
+function sumElfCalories (elves) {
+  let sum = 0
+  for (let i = 0; i < elves.length; i++) {
+    sum += elves[i]
+  }
+  return sum
 }
 
 async function printLargestAmount () {
-  const max = await largestElfCalories('elf.txt')
+  const elves = await largestElfCalories('elf.txt', 3)
+  console.log(elves)
+  const max = sumElfCalories(elves)
   console.log('Elf is carrying: ' + max)
 }
 
