33c930b9473cd2b8636504514bd856dab97c0a84
Zeke <soto.zeke5@gmail.com>
2023-06-20 20:20:41 -0500

    day6 part1 fixes to pass tests

--- day6/com-signals.js
+++ day6/com-signals.js
@@ -18,10 +18,11 @@ const getDataStreamBuffer = function (stream = '') {
   while (markerPos - startingPos < bufferLength && markerPos < stream.length) {
     const currentChar = stream.charAt(markerPos)
     if (charMap.get(currentChar) >= 0) {
-      startingPos = charMap.get(currentChar) + 1
+      startingPos = Math.max(charMap.get(currentChar) + 1, startingPos)
       // charMap.set(currentChar, markerPos)
-    } else if (markerPos >= bufferLength - 1 && charMap.get(stream.charAt(markerPos - bufferLength - 1)) === markerPos - bufferLength - 1) {
-      charMap.set(stream.charAt(markerPos - bufferLength - 1), -1)
+    }
+    if (markerPos >= bufferLength - 1 && charMap.get(stream.charAt(markerPos - bufferLength + 1)) === markerPos - bufferLength + 1) {
+      charMap.set(stream.charAt(markerPos - bufferLength + 1), -1)
       // charMap.set(currentChar, markerPos)
     }
     charMap.set(currentChar, markerPos)
@@ -37,7 +38,8 @@ const getDataStreamBufferFromFile = function (file = '') {
   }
 
   const fs = require('fs')
-  return getDataStreamBufferFromFile(fs.readFileSync(file, 'utf8').toString())
+  const buffer = getDataStreamBufferFromFile(fs.readFileSync(file, 'utf8'))
+  return buffer.toString()
 }
 
 module.exports = {
